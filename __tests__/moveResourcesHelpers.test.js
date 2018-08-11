e382fa9473b9cce8d6895c79d39adc652b60300b
bspidel <bruce.spidel@gmail.com>
2018-08-11 18:17:41 -0400

    added test success criteria

--- __tests__/moveResourcesHelpers.test.js
+++ __tests__/moveResourcesHelpers.test.js
@@ -12,7 +12,7 @@ describe('moveResourcesHelpers', () => {
   let initialFolder;
   let fromFolder;
   let toFolder;
-
+  let res;
   beforeEach(() => {
     // simulate download into sourceFolder
     sourceFolder = path.join(__dirname, 'fixtures', 'resources', 'bs');
@@ -25,45 +25,48 @@ describe('moveResourcesHelpers', () => {
   });
 
   test('moveResources: copy bible books', () => {
-    moveResourcesHelpers.moveResources(fromFolder, toFolder);
+    res = moveResourcesHelpers.moveResources(fromFolder, toFolder);
     const deepFolder = fs.__catMockFS(path.join(
         toFolder, 'bibles', 'v1', 'mat'));
     const chapter1 = JSON.stringify([
       "1.json"
     ], null, 2);
 
+    expect(res).toEqual(true);
     expect(deepFolder).toEqual(chapter1);
   });
 
   test('moveResources: copy lexicons', () => {
-    moveResourcesHelpers.moveResources(fromFolder, toFolder);
+    res = moveResourcesHelpers.moveResources(fromFolder, toFolder);
     const deepFolder = fs.__catMockFS(path.join(
         toFolder, 'lexicons', 'ugl', 'v1', 'content'));
     const aWord = JSON.stringify([
       "beauty.json"
     ], null, 2);
 
+    expect(res).toEqual(true);
     expect(deepFolder).toEqual(aWord);
   });
 
   test('moveResources: copy translation helps', () => {
-    moveResourcesHelpers.moveResources(fromFolder, toFolder);
+    res = moveResourcesHelpers.moveResources(fromFolder, toFolder);
     const deepFolder = fs.__catMockFS(path.join(
         toFolder, 'translationHelps', 'translationAcademy', 'v9', 'process'));
     const share = JSON.stringify([
       "share-content.md"
     ], null, 2);
 
+    expect(res).toEqual(true);
     expect(deepFolder).toEqual(share);
   });
 
   test('moveResources: missing first arg', () => {
-    const res = moveResourcesHelpers.moveResources(null, toFolder);
+    res = moveResourcesHelpers.moveResources(null, toFolder);
     expect(res).toEqual(false);
   });
 
   test('moveResources: missing second arg', () => {
-    const res = moveResourcesHelpers.moveResources(fromFolder);
+    res = moveResourcesHelpers.moveResources(fromFolder);
     expect(res).toEqual(false);
   });
 });
