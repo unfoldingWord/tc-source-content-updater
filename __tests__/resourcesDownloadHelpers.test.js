dca9b86662ae8390949cdc58e6befb4e77ae741e
Richard Mahn <rich.mahn@unfoldingword.org>
2018-08-13 16:06:43 -0600

    Fixes tests

--- __tests__/resourcesDownloadHelpers.test.js
+++ __tests__/resourcesDownloadHelpers.test.js
@@ -2,30 +2,30 @@
 import * as resourcesDownloadHelpers from '../src/helpers/resourcesDownloadHelpers';
 
 describe('Tests for resourcesDownloadHelpers', function() {
-  it('Test resourcesDownloadHelpers.processTranslationAcademy() for null', () => {
+  it('Test resourcesDownloadHelpers.processTranslationAcademy() for null', async () => {
     // given
     const resourceList = null;
     const expectedError = new Error('Resource list empty');
 
     // then
-    expect(resourcesDownloadHelpers.downloadResources(resourceList)).rejects.toEqual(expectedError);
+    await expect(resourcesDownloadHelpers.downloadResources(resourceList)).rejects.toEqual(expectedError);
   });
 
-  it('Test resourcesDownloadHelpers.processTranslationAcademy() for empty list', () => {
+  it('Test resourcesDownloadHelpers.processTranslationAcademy() for empty list', async () => {
     // given
     const resourceList = [];
     const expectedError = new Error('Resource list empty');
 
     // then
-    expect(resourcesDownloadHelpers.downloadResources(resourceList)).rejects.toEqual(expectedError);
+    await expect(resourcesDownloadHelpers.downloadResources(resourceList)).rejects.toEqual(expectedError);
   });
 
-  it('Test resourcesDownloadHelpers.processTranslationAcademy() for populated list', () => {
+  it('Test resourcesDownloadHelpers.processTranslationAcademy() for populated list', async () => {
     // given
     const resourceList = [{languageId: 'en', resourceId: 'ult'}];
     const expectedResolve = 'success';
 
     // then
-    expect(resourcesDownloadHelpers.downloadResources(resourceList)).resolves.toEqual(expectedResolve);
+    await expect(resourcesDownloadHelpers.downloadResources(resourceList)).resolves.toEqual(expectedResolve);
   });
 });
