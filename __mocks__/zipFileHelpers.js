1a6603921a86529cf887021708843c731df6af67
Richard Mahn <rich.mahn@unfoldingword.org>
2018-08-16 06:42:49 -0600

    Adds mock for zipFileHelpers

---
+++ __mocks__/zipFileHelpers.js
@@ -0,0 +1,16 @@
+jest.mock('fs-extra');
+import AdmZip from 'adm-zip';
+import tmp from 'tmp';
+import fs from 'fs-extra';
+
+/**
+ * @description unzips a zip file into the destination path in the mockFS
+ * @param {String} zipFilePath Path to zip file
+ * @param {String} dest Destination path
+ */
+export const extractZipFile = (zipFilePath, dest) => {
+  const zip = new AdmZip(Buffer.from(fs.mockFS[zipFilePath], 'utf8'));
+  const tmpDirObj = tmp.dirSync();
+  zip.extractAllTo(tmpDirObj.name);
+  fs.__loadDirIntoMockFs(tmpDirObj.name, dest);
+};
--- src/helpers/zipFileHelpers.js
+++ src/helpers/zipFileHelpers.js
@@ -4,9 +4,8 @@ import AdmZip from 'adm-zip';
  * @description unzips a zip file into the destination path.
  * @param {String} zipFilePath Path to zip file
  * @param {String} dest Destination path
- * @param {Boolean} overwrite If true, will overwrite existing files
  */
-export const extractZipFile = (zipFilePath, dest, overwrite = true) => {
+export const extractZipFile = (zipFilePath, dest) => {
   const zip = new AdmZip(zipFilePath);
-  zip.extractAllTo(dest, overwrite);
+  zip.extractAllTo(dest);
 };
