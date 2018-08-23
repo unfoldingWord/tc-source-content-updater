1400c75a2cdc637cea5c8a7b6e4fd28fa6ee8b7e
RoyalSix <jay.a.scottjr@biola.edu>
2018-08-23 16:29:58 -0700

    added api helper tests

--- __tests__/apiHelpers.js
+++ __tests__/apiHelpers.js
@@ -1,6 +1,12 @@
-/* eslint-env jest */
-describe('This is a first test', () => {
-  it('should pass', () => {
-    expect(true).toBeTruthy();
+import * as apiHelpers from '../src/helpers/apiHelpers';
+
+describe('apiHelpers.getCatalog', () => {
+  it('should get the resulting catalog', () => {
+    return apiHelpers.getCatalog().then(res => {
+      expect(res).toMatchObject({
+        catalogs: expect.any(Array),
+        subjects: expect.any(Array)
+      });
+    });
   });
 });
