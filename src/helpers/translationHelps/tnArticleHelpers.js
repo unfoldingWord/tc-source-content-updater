1249c47f193928521deb5b43387a463112dbf946
mannycolon <colonmanuel7@gmail.com>
2019-06-25 21:54:19 -0400

    lOGGING tsvRelations

--- src/helpers/translationHelps/tnArticleHelpers.js
+++ src/helpers/translationHelps/tnArticleHelpers.js
@@ -40,8 +40,25 @@ export async function processTranslationNotes(resource, sourcePath, outputPath) 
     fs.removeSync(outputPath);
   }
 
+
+  /**
+   * function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+   */
+
   const tsvManifest = resourcesHelpers.getResourceManifestFromYaml(sourcePath);
-  console.log('tsvManifest', tsvManifest);
+  // array of related resources used to generated the tsv.
+  const tsvRelations = tsvManifest.dublin_core.relation;
+
+  console.log('tsvRelations', tsvRelations);
 
   const tsvFiles = fs.readdirSync(sourcePath).filter((filename) => path.extname(filename) === '.tsv');
 
