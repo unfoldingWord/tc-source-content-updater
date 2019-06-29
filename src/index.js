220255b464c3d6cdcf76a38b0ecd26fb0ce22184
mannycolon <colonmanuel7@gmail.com>
2019-06-29 02:14:37 -0400

    Removed console logs from index.js

--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ Updater.prototype = {};
  */
 Updater.prototype.updateCatalog = async function() {
   this.remoteCatalog = await apiHelpers.getCatalog();
-  console.log('this.remoteCatalog', this.remoteCatalog);
 };
 
 /**
@@ -50,7 +49,6 @@ Updater.prototype.updateCatalog = async function() {
  */
 Updater.prototype.getLatestResources = async function(localResourceList) {
   await this.updateCatalog();
-  console.log('this.remoteCatalog', this.remoteCatalog);
   this.updatedCatalogResources = parseHelpers.getLatestResources(this.remoteCatalog, localResourceList);
   return parseHelpers.getUpdatedLanguageList(this.updatedCatalogResources);
 };
