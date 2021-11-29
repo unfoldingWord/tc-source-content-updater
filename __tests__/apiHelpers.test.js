3dffdf70c93b6971a3855ed8cac797f4083ed353
blm <mcleanb@photo-nomad.net>
2021-11-29 15:05:42 -0500

    add test comparing catalog next with pivoted.json.

--- __tests__/apiHelpers.test.js
+++ __tests__/apiHelpers.test.js
@@ -1,4 +1,8 @@
+import fs from 'fs-extra';
+import path from 'path-extra';
 import * as apiHelpers from '../src/helpers/apiHelpers';
+
+jest.unmock('fs-extra');
 
 describe('apiHelpers.getCatalog', () => {
   it('should get the resulting catalog', () => {
@@ -7,15 +11,161 @@ describe('apiHelpers.getCatalog', () => {
         catalogs: expect.any(Array),
         subjects: expect.any(Array)
       });
+      const items = res && res.subjects;
+      console.log(`D43 Catalog returned ${items.length} total items`);
+      const csvLines = [];
+      const org = 'Door43-Catalog';
+      for (const item of items) {
+        const language = item.language;
+        for (const resource of item.resources) {
+          const resId = resource.identifier;
+          const subject = resource.subject;
+          const repo = `${language}_${resId}`;
+          addCsvItem(csvLines, org, repo, subject, resource);
+        }
+      }
+      console.log(`D43 Catalog flattened has ${csvLines.length} total items`);
+      writeCsv('./CatalogOld.tsv', csvLines);
     });
   });
 });
 
+describe('apiHelpers compare pivoted.json with CN', () => {
+  it('should make a merged CSV', async () => {
+    const res = await apiHelpers.getCatalog();
+    expect(res).toMatchObject({
+      catalogs: expect.any(Array),
+      subjects: expect.any(Array)
+    });
+    const items = res && res.subjects;
+    console.log(`D43 Catalog returned ${items.length} total items`);
+    const csvLines = [];
+    const org = 'Door43-Catalog';
+    const oldCatalog = 'Old';
+    for (const item of items) {
+      const language = item.language;
+      for (const resource of item.resources) {
+        const resId = resource.identifier;
+        const subject = resource.subject;
+        const repo = `${language}_${resId}`;
+        addCsvItem2(csvLines, org, repo, subject, resource, oldCatalog);
+      }
+    }
+    console.log(`D43 Catalog flattened has ${csvLines.length} total items`);
+    writeCsv('./CatalogOld.tsv', csvLines);
+
+    const latestD43Catalog = 'http://git.door43.org/api/catalog/v5?owner=Door43-Catalog&stage=latest';
+    const data = await apiHelpers.doMultipartQuery(latestD43Catalog);
+    console.log(`Catalog Next Found ${data.length} total items`);
+    expect(data.length).toBeTruthy();
+    const cnCatalog = 'CN';
+    const cnCatalogCombined = 'CN+Old';
+    for (const item of data) {
+      const subject = item.subject;
+      const repo = item.name;
+      const org = item.owner;
+      const pos = csvLines.findIndex(line => ((line.repo === repo) && (line.org === org)));
+      if (pos >= 0) {
+        const line = csvLines[pos];
+        if (line.category === oldCatalog) {
+          line.category = cnCatalogCombined;
+        } else {
+          console.log(JSON.stringify(line));
+        }
+      } else {
+        addCsvItem2(csvLines, org, repo, subject, item, cnCatalog);
+      }
+    }
+
+    writeCsv2('./Catalog-CN-and-Old.tsv', csvLines);
+    console.log('done');
+
+  }, 10000);
+});
+
 describe('apiHelpers.getCatalogCN', () => {
   it('should get the CN catalog', async () => {
-    // eslint-disable-next-line no-debugger
-    const data = await apiHelpers.doMultipartQuery('https://git.door43.org/api/catalog/v5/search?subject=Bible%2CAligned%20Bible%2CGreek_New_Testament%2CHebrew_Old_Testament%2CTranslation%20Words%2CTranslation%20Notes%2CTranslation%20Academy&sort=subject&limit=50');
-    console.log(data);
+    const filteredSearch = 'https://git.door43.org/api/catalog/v5/search?subject=Bible%2CAligned%20Bible%2CGreek_New_Testament%2CHebrew_Old_Testament%2CTranslation%20Words%2CTranslation%20Notes%2CTranslation%20Academy&sort=subject&limit=50';
+    const unFilteredSearch = 'https://git.door43.org/api/catalog/v5/search?sort=subject&limit=50';
+    // const latestD43Catalog = 'https://git.door43.org/api/catalog/v5/search/Door43-Catalog?stage=latest&limit=50';
+    const latestD43Catalog = 'http://git.door43.org/api/catalog/v5?owner=Door43-Catalog&stage=latest';
+    const data = await apiHelpers.doMultipartQuery(unFilteredSearch);
+    console.log(`Catalog Next Found ${data.length} total items`);
     expect(data.length).toBeTruthy();
+    const orgs = {};
+    for (let i = 0, l = data.length; i < l; i++) {
+      const item = data[i];
+      const org = item && item.repo && item.repo.owner && item.repo.owner.login || null;
+      if (org) {
+        if (!orgs[org]) {
+          orgs[org] = [];
+        }
+        orgs[org].push(item);
+      } else {
+        console.log(`missing org in ${JSON.stringify(item)}`);
+      }
+    }
+    const orgList = Object.keys(orgs);
+    for (const org of orgList) {
+      console.log(`${org} has ${orgs[org].length} items`);
+    }
+
+    let csvLines = [];
+    const org = 'Door43-Catalog';
+    getOrgItems(orgs, org, csvLines);
+    console.log(`D43 Catalog flattened has ${csvLines.length} total items`);
+    writeCsv('./CatalogCN-D43.tsv', csvLines);
+
+    csvLines = [];
+    for (const org of orgList) {
+      getOrgItems(orgs, org, csvLines);
+    }
+    console.log(`Catalog Next flattened has ${csvLines.length} total items`);
+    writeCsv('./CatalogCN.tsv', csvLines);
   });
-});
+}, 10000);
+
+//
+// helpers
+//
+
+function addCsvItem(list, org, repo, subject, item) {
+  const itemJson = JSON.stringify(item).replace('\t','\\t');
+  list.push({org, repo, subject, resource: itemJson});
+  // list.push(`${org}\t${repo}\t${subject}\t${itemJson}`);
+}
+
+function addCsvItem2(list, org, repo, subject, item, category) {
+  const itemJson = JSON.stringify(item).replace('\t','\\t');
+  list.push({category, org, repo, subject, resource: itemJson});
+  // list.push(`${org}\t${repo}\t${subject}\t${itemJson}`);
+}
+
+function writeCsv(filename, list) {
+  const csvLines = [];
+  for (const item of list) {
+    const itemJson = JSON.stringify(item.resource).replace('\t', '\\t');
+    csvLines.push(`${item.org}\t${item.repo}\t${item.subject}\t${itemJson}`);
+  }
+  fs.writeFileSync(filename, csvLines.join('\n') + '\n', 'utf8');
+}
+
+function writeCsv2(filename, list) {
+  const csvLines = [];
+  for (const item of list) {
+    const itemJson = JSON.stringify(item.resource).replace('\t', '\\t');
+    csvLines.push(`${item.category}\t${item.org}\t${item.repo}\t${item.subject}\t${itemJson}`);
+  }
+  fs.writeFileSync(filename, csvLines.join('\n') + '\n', 'utf8');
+}
+
+function getOrgItems(orgs, org, csvLines) {
+  const items = orgs[org];
+  for (const item of items) {
+    const subject = item.subject;
+    const repo = item.name;
+    const org = item.owner;
+    addCsvItem(csvLines, org, repo, subject, item);
+  }
+}
+
