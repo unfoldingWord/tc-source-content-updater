15770a7e5176bc992a2a270f4ba3cb98f8a485ff
blm <mcleanb@photo-nomad.net>
2022-07-07 08:55:28 -0400

    fix for downloading metadata

--- src/helpers/resourcesDownloadHelpers.js
+++ src/helpers/resourcesDownloadHelpers.js
@@ -122,7 +122,7 @@ export const downloadAndProcessResource = async (resource, resourcesPath, downlo
     const latest = await getReleaseMetaData(owner, resourceData.name, tag);
     const release = latest && latest.release;
     let version = release && release.tag_name;
-    if (!version) {
+    if (!version) { // if no release found, then get version from manifest data
       const manifest = await downloadManifestData(owner, resourceData.name, tag);
       version = manifest && manifest.dublin_core && manifest.dublin_core.version;
     }
