c88c6b04fd808c0a0b858ea4fe6ce681c59e456e
blm <mcleanb@photo-nomad.net>
2022-09-16 16:24:42 -0400

    fix for missing repo name on download

--- src/helpers/resourcesDownloadHelpers.js
+++ src/helpers/resourcesDownloadHelpers.js
@@ -209,8 +209,8 @@ export const downloadAndProcessResource = async (resource, resourcesPath, downlo
     const latest = await getReleaseMetaData(owner, repo, tag);
     const release = latest && latest.release;
     let version = release && release.tag_name;
+    resource.downloadUrl = release && release.zipball_url || latest.zipball_url;
     if (!version) { // if no release found, then get version from manifest data
-      resource.downloadUrl = latest.zipball_url;
       const manifest = await downloadManifestData(owner, repo, tag);
       version = manifest && manifest.dublin_core && manifest.dublin_core.version;
     }
